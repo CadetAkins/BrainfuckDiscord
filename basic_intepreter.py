536236216a4697b2101bcafab2ef2e28fe644317
Akins <82357502+Akins2229@users.noreply.github.com>
2021-10-20 20:03:00 -0700

    Create basic_intepreter.py

---
+++ basic_intepreter.py
@@ -0,0 +1,35 @@
+# A basic interpreter to add functions to systemf brainfuck
+# Python 3.8
+
+file = input()
+with open(file, 'r+') as f:
+  functions = []
+  i = 0
+  lines = f.read().split(";")
+  for i in lines: 
+    if i.startswith("$"):
+      if len(i.split) > 1: #makes sure it isn't just a function call
+        functions.append([i.split(" | ")[0], i.split(" | ")[1]], i) # store lines number for possible future debug purposes
+        lines.remove(i)
+        i += 1
+      else:
+        i += 1
+        continue
+     else:
+      i += 1
+      continue
+      
+      
+ with open(file, 'r+') as f:
+  file_string = f.read()
+  for x in functions:
+    file_string.replace(x[0], x[1])
+    
+   
+      
+
+#Example Code
+
+# $function_name | >+++++++++[<++++++++>-]<.      prints ASCII value for H (line does not execute until called.)
+#
+#$function_name                                   calls function to print 
