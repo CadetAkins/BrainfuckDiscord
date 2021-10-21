1192c826cd279b98847f4fdaea9b5648866ff31d
Akins <82357502+Akins2229@users.noreply.github.com>
2021-10-20 20:05:35 -0700

    Update basic_intepreter.py

--- basic_intepreter.py
+++ basic_intepreter.py
@@ -5,11 +5,11 @@ file = input()
 with open(file, 'r+') as f:
   functions = []
   i = 0
-  lines = f.read().split(";")
+  lines = f.read().replace(" ", "").replace("\n", "").replace("\t", "").split(";")
   for i in lines: 
     if i.startswith("$"):
       if len(i.split) > 1: #makes sure it isn't just a function call
-        functions.append([i.split(" | ")[0], i.split(" | ")[1]], i) # store lines number for possible future debug purposes
+        functions.append([i.split("|")[0], i.split("|")[1]], i) # store lines number for possible future debug purposes
         lines.remove(i)
         i += 1
       else:
